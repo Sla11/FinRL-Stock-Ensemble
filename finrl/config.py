7d55552fe42143899b09075f8b1243b5a32e16df
Sla11 <100123095+Sla11@users.noreply.github.com>
2023-12-08 16:30:02 +0700

    Update config.py
    new indicators

--- finrl/config.py
+++ finrl/config.py
@@ -46,19 +46,19 @@ TRADE_END_DATE = "2021-12-01"
 # ]
 
 INDICATORS = {
-    "macd": lambda df: talib.MACD(df['close'])[0],  # MACD
-    "boll_ub": lambda df: talib.BBANDS(df['close'])[0],  # Upper Bollinger Bands
-    "boll_lb": lambda df: talib.BBANDS(df['close'])[2],  # Lower Bollinger Bands
-    "rsi_30": lambda df: talib.RSI(df['close'], timeperiod=30),
-    "cci_30": lambda df: talib.CCI(df['high'], df['low'], df['close'], timeperiod=30),
-    "dx_30": lambda df: talib.DX(df['high'], df['low'], df['close'], timeperiod=30),
-    "close_30_sma": lambda df: talib.SMA(df['close'], timeperiod=30),
-    "close_60_sma": lambda df: talib.SMA(df['close'], timeperiod=60),
-    "aroon": lambda df: talib.AROON(df['high'], df['low'])[0],  # Aroon up
-    "kdjk": lambda df: talib.STOCH(df['high'], df['low'], df['close'])[0],  # Stochastic %K
-    "kdjd": lambda df: talib.STOCH(df['high'], df['low'], df['close'])[1],  # Stochastic %D
-    "kdjj": lambda df: 3 * df['kdjk'] - 2 * df['kdjd'],  # J line not directly available in talib
-    "trix": lambda df: talib.TRIX(df['close']),
+    # "macd": lambda df: talib.MACD(df['close'])[0],  # MACD
+    # "boll_ub": lambda df: talib.BBANDS(df['close'])[0],  # Upper Bollinger Bands
+    # "boll_lb": lambda df: talib.BBANDS(df['close'])[2],  # Lower Bollinger Bands
+    # "rsi_30": lambda df: talib.RSI(df['close'], timeperiod=30),
+    # "cci_30": lambda df: talib.CCI(df['high'], df['low'], df['close'], timeperiod=30),
+    # "dx_30": lambda df: talib.DX(df['high'], df['low'], df['close'], timeperiod=30),
+    # "close_30_sma": lambda df: talib.SMA(df['close'], timeperiod=30),
+    # "close_60_sma": lambda df: talib.SMA(df['close'], timeperiod=60),
+    # "aroon": lambda df: talib.AROON(df['high'], df['low'])[0],  # Aroon up
+    # "kdjk": lambda df: talib.STOCH(df['high'], df['low'], df['close'])[0],  # Stochastic %K
+    # "kdjd": lambda df: talib.STOCH(df['high'], df['low'], df['close'])[1],  # Stochastic %D
+    # "kdjj": lambda df: 3 * df['kdjk'] - 2 * df['kdjd'],  # J line not directly available in talib
+    # "trix": lambda df: talib.TRIX(df['close']),
     # Add more indicators as needed
 
     # New indicators
